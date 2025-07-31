ff73abd6adf678ea66236c046f3ae51abe2cdb4f
Bashir Muhammed <bashmufol@gmail.com>
2025-07-31 20:31:16 +0100

    updated

--- src/main/java/com/bash/springaiproject/ChatController.java
+++ src/main/java/com/bash/springaiproject/ChatController.java
@@ -1,4 +1,18 @@
 package com.bash.springaiproject;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ChatController {
+    private final ChatService chatService;
+
+    @GetMapping("/ask-ai")
+    public String getResponse(@RequestParam String prompt) {
+        return chatService.getResponse(prompt);
+    }
+
 }
--- src/main/java/com/bash/springaiproject/ChatService.java
+++ src/main/java/com/bash/springaiproject/ChatService.java
@@ -2,7 +2,9 @@ package com.bash.springaiproject;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class ChatService {
     private final ChatModel chatModel;
